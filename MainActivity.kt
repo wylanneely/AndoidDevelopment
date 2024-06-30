5cf1851e3b9d2a6d897bb4fd40fcae25ec8cf160
Wylan Neely <wylanneely@gmail.com>
2024-06-29 18:18:04 -0700

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -5,19 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
 import com.wylan.apps.realtimeweather.ui.theme.RealtimeWeatherTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val weatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
+
         enableEdgeToEdge()
         setContent {
             RealtimeWeatherTheme {
@@ -25,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WeatherPage()
+                    WeatherPage(weatherViewModel)
                 }
 
             }
--- WeatherPage.kt
+++ WeatherPage.kt
@@ -1,30 +1,178 @@
 package com.wylan.apps.realtimeweather
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import coil.compose.AsyncImage
+import com.wylan.apps.realtimeweather.API.NetworkResponse
+import com.wylan.apps.realtimeweather.API.WeatherResponseModel
+
 
 @Composable
-fun WeatherPage() {
+fun WeatherPage(weatherViewModel: WeatherViewModel) {
 
     var city by remember {
         mutableStateOf("")
     }
 
-    Column { 
-        Row {
-            OutlinedTextField(value = city
-                , onValueChange = {city = it},
+    val weatherResult = weatherViewModel.weatherResult.observeAsState()
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Spacer(modifier = Modifier.height(8.dp))
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement =  Arrangement.SpaceEvenly
+        ){
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = city,
+                onValueChange = {city = it},
                 label = {
                     Text(text = "Search for any location")
                 })
+            IconButton(onClick = {
+                weatherViewModel.getData(city)
+                keyboardController?.hide()
+            }) {
+                Icon(imageVector = Icons.Default.Search,
+                    contentDescription = "Search for any location")
+            }
         }
-        
+    when(val result = weatherResult.value) {
+        is NetworkResponse.Error -> {
+            Text(text = result.message)
+        }
+        NetworkResponse.Loading -> {
+            CircularProgressIndicator()
+        }
+        is NetworkResponse.Success -> {
+            WeatherDetails(data = result.data)
+        }
+        null -> {}
     }
+
+    }
+}
+@Composable
+fun WeatherDetails(data : WeatherResponseModel ) {
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Icon (
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Location Icon",
+                modifier = Modifier.size(40.dp)
+            )
+            Text(text = data.location.name, fontSize = 30.sp)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = data.location.region, fontSize = 18.sp, color = Color.Gray)
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = " ${data.current.temp_f} °F",
+            fontSize = 56.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        AsyncImage(
+            modifier = Modifier.size(160.dp),
+            model = "https:${data.current.condition.icon}".replace("64x64","128x128"),
+            contentDescription = "Condition Icon"
+        )
+        Text(text = data.current.condition.text,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Center,
+            color = Color.Gray
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Card {
+            Column (
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("Humidity", value = data.current.humidity)
+                    WeatherKeyVal("Wind Speed", value = data.current.wind_mph + " mph" )
+                }
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("Feels Like", value = "${data.current.feelslike_f} °F")
+                    WeatherKeyVal("Wind Direction", value = data.current.wind_dir )
+                }
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    WeatherKeyVal("Local Time", value = data.location.localtime.split(" ")[1])
+                    WeatherKeyVal("Local Date", value = data.location.localtime.split(" ")[0])
+                }
+            }
+        }
+    }
+
+}
+
+
+@Composable
+fun WeatherKeyVal(key: String, value: String) {
+    Column (
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = value, fontSize = 24.sp, fontWeight = FontWeight.Bold )
+        Text(text = key, fontWeight = FontWeight.SemiBold, color = Color.Gray )
+    }
+
 }
