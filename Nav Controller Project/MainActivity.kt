287e6ef712949b9dce7cd7657889442e02338d7d
Wylan Neely <wylanneely@gmail.com>
2024-06-30 13:23:34 -0700

    Created NavController Project

---
+++ Nav Controller Project/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.wylan.apps.realtimeweather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
+import com.wylan.apps.realtimeweather.ui.theme.RealtimeWeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val weatherViewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
+
+        enableEdgeToEdge()
+        setContent {
+            RealtimeWeatherTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WeatherPage(weatherViewModel)
+                }
+
+            }
+        }
+    }
+}
