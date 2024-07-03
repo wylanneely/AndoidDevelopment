f0d61c99c7f9279a84edecf939d8c9b0089b752c
Wylan Neely <wylanneely@gmail.com>
2024-07-02 18:17:46 -0700

    Add files via upload

--- LaxyColumnExample/ListDemo.kt
+++ LaxyColumnExample/ListDemo.kt
@@ -1,15 +1,25 @@
 package com.wylan.apps.listdemo
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,15 +32,42 @@ fun ListDemo() {
 
 @Composable
 fun LazyColumnDemo() {
-    var myList = listOf<String>("A","B","C","D","E")
-     LazyColumn(content = {
+    var scrollState = rememberScrollState()
+    var myList = getAllQuoteResources()
+     LazyColumn(
+         modifier = Modifier.verticalScroll(scrollState),
+         content = {
         itemsIndexed(myList, itemContent = { index, item ->
-            TextItem(text = "Item $item")
+            QuoteViewItem(quoteResource = item)
         })
     })
 }
 
-
+@Composable
+fun QuoteViewItem(quoteResource: QuoteResource){
+    var context = LocalContext.current
+    Row(
+       modifier = Modifier
+           .fillMaxWidth()
+           .padding(16.dp)
+           .clickable {
+               Toast
+                   .makeText(context, "Clicked ${quoteResource.author}", Toast.LENGTH_SHORT)
+                   .show()
+           }
+    ){
+        Image(painter = painterResource(id = quoteResource.imageRes),
+            contentDescription = quoteResource.author,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(64.dp)
+                .scale(1.0f)
+            )
+        Column {
+            Text(text = quoteResource.author)
+        }
+    }
+}
 
 @Composable
 fun SimpleColumn() {
